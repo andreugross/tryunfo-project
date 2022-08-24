d3dacf3c6620f133bb597d98414ad784bf9d3083
André Gross <andgross@gmail.com>
2022-08-24 15:21:16 -0300

    Update: Props ok, correção de lint no primeiro req. requisito #02, done

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,14 +1,35 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Forms extends Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
     return (
       <div className="add-card">
         <h1>Adicionar nova carta</h1>
         <form>
           <label htmlFor="name">
             Nome
-            <input type="text" data-testid="name-input" name="nome" id="name" />
+            <input
+              type="text"
+              data-testid="name-input"
+              id="name"
+              value={ cardName }
+              onChange={ onInputChange }
+            />
           </label>
 
           <label htmlFor="description">
@@ -16,8 +37,9 @@ export default class Forms extends Component {
             <input
               type="textarea"
               data-testid="description-input"
-              name="nome"
               id="description"
+              value={ cardDescription }
+              onChange={ onInputChange }
             />
           </label>
 
@@ -26,8 +48,9 @@ export default class Forms extends Component {
             <input
               type="number"
               data-testid="attr1-input"
-              name="attr01"
               id="attr01"
+              value={ cardAttr1 }
+              onChange={ onInputChange }
             />
           </label>
 
@@ -36,8 +59,9 @@ export default class Forms extends Component {
             <input
               type="number"
               data-testid="attr2-input"
-              name="attr02"
               id="attr02"
+              value={ cardAttr2 }
+              onChange={ onInputChange }
             />
           </label>
 
@@ -46,8 +70,9 @@ export default class Forms extends Component {
             <input
               type="number"
               data-testid="attr3-input"
-              name="attr03"
               id="attr03"
+              value={ cardAttr3 }
+              onChange={ onInputChange }
             />
           </label>
 
@@ -56,14 +81,20 @@ export default class Forms extends Component {
             <input
               type="text"
               ddata-testid="image-input"
-              name="image"
               id="image"
+              value={ cardImage }
+              onChange={ onInputChange }
             />
           </label>
 
           <label htmlFor="rare">
             Raridade
-            <select name="rare" id="rare" data-testid="rare-input">
+            <select
+              id="rare"
+              data-testid="rare-input"
+              value={ cardRare }
+              onChange={ onInputChange }
+            >
               <option value="normal">normal</option>
               <option value="rare">raro</option>
               <option value="very-rare">muito raro</option>
@@ -72,11 +103,23 @@ export default class Forms extends Component {
 
           <label htmlFor="trunfo">
             Super Trybe Trunfo
-            <input type="checkbox" value="trunfo" name="trunfo" id="trunfo" />
+            <input
+              type="checkbox"
+              value="trunfo"
+              id="trunfo"
+              data-testid="trunfo-input"
+              checked={ cardTrunfo }
+              onChange={ onInputChange }
+            />
           </label>
 
           <div>
-            <button type="submit" data-testid="save-button">
+            <button
+              type="submit"
+              data-testid="save-button"
+              disabled={ isSaveButtonDisabled }
+              onClick={ onSaveButtonClick }
+            >
               Salvar
             </button>
           </div>
@@ -85,3 +128,18 @@ export default class Forms extends Component {
     );
   }
 }
+
+Forms.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
